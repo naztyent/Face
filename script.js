53f3923999f307c67c583bc159be7adab981dfd5
dagnazty <38539274+dagnazty@users.noreply.github.com>
2024-01-23 22:41:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const emoticons = ["< ⚆_⚆>", "<☉_☉ >", "< ◕‿◕>", "<◕‿◕ >", "<⇀‿‿↼>", "<≖‿‿≖>", "<◕‿‿◕>", "<-__->", "<°▃▃°>", "<⌐■_■>", "<•‿‿•>", "<ᵔ◡◡ᵔ>", "<^‿‿^>", "<☼‿‿☼>", "<≖__≖>", "<✜‿‿✜>", "<ب__ب>", "<╥☁╥ >", "<-_-'>", "<♥‿‿♥>", "<☓‿‿☓>", "<#__#>", "<1__0>", "<1__1>", "<0__1>"];
+const emoticonElement = document.getElementById('emoticon');
+
 function changeEmoticon() {
-    const randomIndex = Math.floor(Math.random() * emoticons.length);
-    document.getElementById('emoticon').innerText = emoticons[randomIndex];
+    // Fade out the emoticon
+    emoticonElement.style.opacity = 0;
+
+    setTimeout(() => {
+        // Get a random emoticon
+        const randomIndex = Math.floor(Math.random() * emoticons.length);
+        emoticonElement.innerText = emoticons[randomIndex];
+
+        // Fade in the new emoticon
+        emoticonElement.style.opacity = 1;
+    }, 500); // This should match half of the transition time in CSS
 }
-setInterval(changeEmoticon, 2000); // Change emoticon every 2 seconds
+
+// Initial display and fade-in of the first emoticon
+changeEmoticon();
+setInterval(changeEmoticon, 3000); // Change emoticon every 3 seconds
+
+document.body.addEventListener('click', function() {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'block'; // Show the notification
+
+    // Hide the notification after 3 seconds
+    setTimeout(function() {
+        notification.style.display = 'none';
+    }, 3000);
+});
